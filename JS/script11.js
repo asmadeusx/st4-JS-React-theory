ec5ea6c9df7d4bfd22fbef72965ea4046fa5b0da
Mikhail <Virus1231@yandex.ru>
2021-01-18 16:05:14 +0300

    020
    020

--- JS/script11.js
+++ JS/script11.js
@@ -9,17 +9,32 @@ const options = {
     colors: {
         border: 'blue',
         bg: 'red'
+    },
+    maketest: function() {
+        console.log('test'); // Добавленный метод нашему объекту.
     }
 };
 // delete options.colors.border;
 // console.log(options.colors);
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof (options[key]) == 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
 
-for (let key in options) {
-    if (typeof (options[key]) == 'object') {
-        for (let i in options[key]) {
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-        }
-    } else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-    }
-}
+// console.log('Кол-во свойств в объекте: ' + counter);
+
+// console.log(Object.keys(options)); //возвращает массив с перечнем имен всех ключей(элементов) в объекте
+// console.log(Object.keys(options).length); //возвращает число всех ключей(элементов) в объекте
+
+// options.maketest();
+
+const {border, bg} = options.colors; // Деструктуризация объекта
+console.log(border);
