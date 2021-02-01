1e3ed907b42df3a6f52e2a4ca2a7984f38df00cd
Mikhail <Virus1231@yandex.ru>
2021-02-01 10:02:16 +0300

    030
    030

--- JS/script.js
+++ JS/script.js
@@ -12,7 +12,7 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-import imageURL from '../img/bg.jpg'
+// import imageURL from '../img/bg.jpg';  // для сборщика Parcel'а
 
 const movieDB = {
     movies: [
@@ -20,14 +20,16 @@ const movieDB = {
         "Лига справедливости",
         "Ла-ла лэнд",
         "Одержимость",
-        "Скотт Пилигрим против..."
+        "Скотт Пилигрим против...",
+        "Кин-дза-дза",
+        "Бэтмен"
     ]
 };
 
 const adv = document.querySelectorAll('.promo__adv img');
 const poster = document.querySelector('.promo__bg');
 const genre = poster.querySelector('.promo__genre');
-const filmlist = document.querySelector('.promo__interactive-list');
+const movieList = document.querySelector('.promo__interactive-list');
 
 
 adv.forEach(item => {
@@ -36,6 +38,19 @@ adv.forEach(item => {
 
 genre.textContent = 'ДРАМА';
 
-poster.style.backgroundImage = imageURL;
+poster.style.backgroundImage = 'url("./img/bg.jpg")';
 
-filmlist.innerHTML = '';
+movieList.innerHTML = '';
+
+movieDB.movies.sort(); // Сортировка массива по алфавиту
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i+1}. ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
+
+// a = a + 1 тоже самое что и a += 1
+// a = a + 'fff' тоже самое что и a += 'fff'
