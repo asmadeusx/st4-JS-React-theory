b8e2d80b91327a7f3f5ea72360a7ad01eca206d3
Mikhail <Virus1231@yandex.ru>
2021-02-11 16:16:11 +0300

    042
    042

--- 042-Параметры документа, окна и работа с ними/script.js
+++ 042-Параметры документа, окна и работа с ними/script.js
@@ -13,6 +13,20 @@ console.log('=================');
 console.log(ofwidth, ofheight);
 
 btn.addEventListener('click', () => {
-    // box.style.height = box.scrollHeight + 'px';
-    console.log(box.scrollTop);
+    // box.style.height = box.scrollHeight + 'px'; // изменить высоту окна по высоте текста
+    console.log(box.scrollTop); // Сколько пикселей пролистано
 });
+
+// Получение всех координат выбранного объекта
+console.log(box.getBoundingClientRect());
+
+// Получение нужной координаты выбранного объекта
+console.log(box.getBoundingClientRect().top);
+
+// отобразить примененные стили к объекуты
+const style = window.getComputedStyle(box);
+console.log(style);
+
+// отобразить конкретный параметр стиля объекта
+console.log(style.display); 
+
